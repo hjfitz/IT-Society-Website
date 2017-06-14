5b56ad262d3e565bed54aa2420a159983238aa5c
Harry Fitzgerald <up780461@myport.ac.uk>
2017-06-14 15:00:09 +0100

    begin work on /about

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const committeeInfo = require('../data/soc.json');
+const router = express.Router();
+
+console.log(committeeInfo);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'IT Soc' });
 });
+
+router.get('/about', (req, res) => {
+  res.render('about', {
+    title: 'About',
+    about: 'class=\'hidden\'',
+    members: committeeInfo
+  })
+})
 
 module.exports = router;

f9945046139a3c09ffa2153239ba3806bf45fe82
Harry Fitzgerald <up780461@myport.ac.uk>
2017-06-28 14:45:27 +0100

    add schema and basic validation. login will be implemented at the end of different pages

--- routes/api/admin/events.js
+++ routes/api/admin/events.js
@@ -4,7 +4,12 @@ const sqlite3 = require('sqlite3');
 eventApi = express.Router();
 
 eventApi.get('/', (req, res) => {
-  res.send('welcome to admin api - events');
+  res.send('Welcome to event API')
+});
+
+eventApi.post('/', (req, res) => {
+  console.log(req.body);
+  res.json({ 'cod':200 });
 });
 
 module.exports = eventApi;

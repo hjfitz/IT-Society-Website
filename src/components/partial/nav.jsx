89c947f2508f542fb2c32963387b7244a195df71
Harry <hfitz83@gmail.com>
2017-07-30 20:02:58 +0100

    center nav logo, add cool transition. move nav to stateful component

--- src/components/partial/nav.jsx
+++ src/components/partial/nav.jsx
@@ -3,14 +3,30 @@ import propTypes from 'prop-types';
 import { Navbar, NavItem } from 'react-materialize';
 import { Link } from 'react-router-dom';
 
-const Nav = (props) => {
-  return (
-    <Navbar className={`center ${props.className}`} brand="The IT Society" right fixed>
-      <NavItem><Link to="/events">Events</Link></NavItem>
-      <NavItem><Link to="/about">About</Link></NavItem>
-    </Navbar>
-  );
-};
+class Nav extends React.Component {
+
+  componentDidMount() {
+    const nav = document.querySelector('nav');
+    window.addEventListener('scroll', () => {
+      const scrollPos = document.body.scrollTop;
+      if (scrollPos > 250) {
+        nav.classList = 'center grey lighten-5';
+      } else {
+        nav.classList = 'center transparent';
+      }
+    });
+  }
+
+  render() {
+    return (
+      <Navbar className={`center ${this.props.className}`} brand="The IT Society" right fixed>
+        <NavItem><Link to="/events">Events</Link></NavItem>
+        <NavItem><Link to="/about">About</Link></NavItem>
+      </Navbar>
+    );
+  }
+}
+
 
 Nav.propTypes = {
   color: propTypes.string.isRequired,
--- src/pages/layout.jsx
+++ src/pages/layout.jsx
@@ -5,7 +5,7 @@ import * as Partial from '../components/partial';
 const Layout = (props) => {
   return (
     <div className="main-container">
-      <Partial.Nav className="transparent-nav" />
+      <Partial.Nav className="transparent" />
       <div className="container">
         {props.children}
       </div>

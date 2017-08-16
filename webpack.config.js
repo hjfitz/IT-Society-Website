5aea8536dee17068a8c4f8f7def2ff0143f7cdd2
Harry <hfitz83@gmail.com>
2017-08-16 20:55:55 +0100

    remove stage 1

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ const config = {
       exclude: /node_modules/,
       loader: 'babel-loader',
       query: {
-        presets: ['es2015', 'react', 'stage-1'],
+        presets: ['es2015', 'react'],
       },
     }],
   },
